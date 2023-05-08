b25073825014a42764ea9991b85092461c2f2652
SAKURALIX <sakura.xlix@gmail.com>
2023-05-08 20:19:43 +0300

    feat: in task-04 add make-and-delete-folder function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,45 @@
+const fs = require('fs');
 
+// fs.mkdir('./04-copy-directory/files-copy', err => {
+//   if(err) throw err;
+// })
+
+fs.readdir('./04-copy-directory/files-copy', (err, files) => {
+  if(err) {
+    fs.mkdir('./04-copy-directory/files-copy', err => {
+      if(err) throw err;
+    });
+  } else if(files.length > 0) {
+  //   fs.rmdir('./04-copy-directory/files-copy', err => {
+  //     if(err) throw err;
+  //     console.log('Папка успешно удалена');
+  //  });
+   console.log('Папка не пуста');
+  } else {
+        fs.rmdir('./04-copy-directory/files-copy', err => {
+          if(err) throw err;
+          console.log('Папка успешно удалена');
+        });
+  }
+});
+
+
+
+
+
+// fs.readdir('./04-copy-directory/files-copy', (err, files) => {
+//   if(err) throw err;
+//   console.log(files.length);
+// });
+
+
+
+
+// fs.copyFile("./04-copy-directory/files/test-css.css", "./04-copy-directory/test-css.css", (err) => {
+//     if (err) {
+//       console.log("Error Found:", err);
+//     }
+//     else {
+//         return
+//     }
+//   });
