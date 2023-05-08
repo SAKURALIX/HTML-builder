fe9093d01e8c27390823c68c062958491daba68d
SAKURALIX <sakura.xlix@gmail.com>
2023-05-08 07:46:25 +0300

    feat: add task-03 fully completed

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,8 +1,5 @@
-const { rejects } = require('assert');
 const fs = require('fs');
 const path = require('node:path');
-const { resolve } = require('path');
-const { stat } = require('fs');
 
 fs.readdir("./03-files-in-folder/secret-folder", {withFileTypes: true}, function(err, files) {
     if(err) {
@@ -10,31 +7,18 @@ fs.readdir("./03-files-in-folder/secret-folder", {withFileTypes: true}, function
     }
     files.forEach(function(file) {
         if(file.isDirectory() === false) {
-            // function getFileSize(name) {
-            //     return new Promise((resolve, reject) => stat(name, (err, { size }) => {
-            //         if(err) return reject(err);
-            //         resolve(size);
-            //     }));
-            // }
             let fileName = file.name;
-
-            // fs.stat('./03-files-in-folder/secret-folder/data.csv', (err, stats) => {
-            //     if(err) {
-            //         console.log(`File doesn't exist.`)
-            //     } else {
-            //         const size = stats.size;
-            //         console.log(stats)
-            //         console.log(size)
-            //     }
-            // })
-
             let dotIndex = fileName.indexOf('.');
             let extention = path.extname(file.name);
-            // let fileSize = getFileSize(file);
-            let fileSize = '';
-            // console.log(file.path)
-            console.log(fileName.slice(0, dotIndex) + ' - ' + extention.slice(1) + fileSize);
-            // console.log(path.extname(file.name));
+
+            fs.stat(`./03-files-in-folder/secret-folder/${fileName}`, (err, stats) => {
+                if(err) {
+                    console.log(`File doesn't exist.`)
+                } else {
+                    let fileSize = (stats.size*0.001).toFixed(3);
+                    return console.log(fileName.slice(0, dotIndex) + ' - ' + extention.slice(1) + ' - ' + fileSize + 'kb');
+                }
+            })
         }
     });
 });
