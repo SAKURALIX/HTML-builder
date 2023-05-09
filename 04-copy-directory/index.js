f6b5ce7a1bbe73eccdc49c6e38c4287cf772c127
SAKURALIX <sakura.xlix@gmail.com>
2023-05-09 06:54:01 +0300

    feat: task-04 fully completed

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,20 +1,52 @@
 const fs = require('fs');
 
-// fs.mkdir('./04-copy-directory/files-copy', err => {
-//   if(err) throw err;
-// })
-
-fs.readdir('./04-copy-directory/files-copy', (err, files) => {
+fs.readdir('./04-copy-directory/files-copy', function copyFiles(err, copiedFiles) {
   if(err) {
     fs.mkdir('./04-copy-directory/files-copy', err => {
       if(err) throw err;
     });
-  } else if(files.length > 0) {
-  //   fs.rmdir('./04-copy-directory/files-copy', err => {
-  //     if(err) throw err;
-  //     console.log('Папка успешно удалена');
-  //  });
-   console.log('Папка не пуста');
+
+    fs.readdir('./04-copy-directory/files', {withFileTypes: true}, (err, files) => {
+      if(err) {
+        throw err
+      } else {
+        files.forEach(function(file) {
+          let fileName = file.name;
+          fs.copyFile(`./04-copy-directory/files/${fileName}`, `./04-copy-directory/files-copy/${fileName}`, err => {
+            if(err) throw err;
+          })
+        });
+      }
+    });
+  } else if(copiedFiles.length > 0) {
+    fs.readdir('./04-copy-directory/files-copy', {withFileTypes: true}, (err, filesToDelete) => {
+      if(err) {
+        throw err
+      } else {
+        filesToDelete.forEach(function(fileToDelete) {
+          let fileName = fileToDelete.name;
+          fs.unlink(`./04-copy-directory/files-copy/${fileName}`, err => {
+            if(err) throw err;
+          })
+        });
+
+        //по новой копирование:
+        fs.readdir('./04-copy-directory/files', {withFileTypes: true}, (err, files) => {
+          if(err) {
+            throw err
+          } else {
+            files.forEach(function(file) {
+              let fileName = file.name;
+              fs.copyFile(`./04-copy-directory/files/${fileName}`, `./04-copy-directory/files-copy/${fileName}`, err => {
+                if(err) throw err;
+              })
+            });
+          }
+        });
+    
+      }
+    });
+
   } else {
         fs.rmdir('./04-copy-directory/files-copy', err => {
           if(err) throw err;
@@ -22,24 +54,3 @@ fs.readdir('./04-copy-directory/files-copy', (err, files) => {
         });
   }
 });
-
-
-
-
-
-// fs.readdir('./04-copy-directory/files-copy', (err, files) => {
-//   if(err) throw err;
-//   console.log(files.length);
-// });
-
-
-
-
-// fs.copyFile("./04-copy-directory/files/test-css.css", "./04-copy-directory/test-css.css", (err) => {
-//     if (err) {
-//       console.log("Error Found:", err);
-//     }
-//     else {
-//         return
-//     }
-//   });
