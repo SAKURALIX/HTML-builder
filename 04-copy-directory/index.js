268fd8e3c6519b4f61314371278b3dac984777d2
SAKURALIX <sakura.xlix@gmail.com>
2023-05-10 05:40:59 +0300

    feat: task-04 add comments

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 
+// readdir пытается прочитать содержимое папки files-copy
 fs.readdir('./04-copy-directory/files-copy', function copyFiles(err, copiedFiles) {
+  // если папка отсутствует и он по идее должен выкинуть ошибку, то mkdir создаёт эту папку
   if(err) {
     fs.mkdir('./04-copy-directory/files-copy', err => {
       if(err) throw err;
     });
 
+    // читает содержимое папки files и копирует каждый файл в созданную папку files-copy
     fs.readdir('./04-copy-directory/files', {withFileTypes: true}, (err, files) => {
       if(err) {
         throw err
@@ -19,6 +22,9 @@ fs.readdir('./04-copy-directory/files-copy', function copyFiles(err, copiedFiles
       }
     });
   } else if(copiedFiles.length > 0) {
+    // если "copiedFiles.length > 0", значит папка files-copy уже существует и содержит файлы
+    // тогда с помощью readdir читает содержимое папки, {withFileTypes: true} определяет, что это файл, а не папка
+    // и удаляет каждый из файлов
     fs.readdir('./04-copy-directory/files-copy', {withFileTypes: true}, (err, filesToDelete) => {
       if(err) {
         throw err
@@ -30,7 +36,7 @@ fs.readdir('./04-copy-directory/files-copy', function copyFiles(err, copiedFiles
           })
         });
 
-        //по новой копирование:
+        //по новой копирует чтобы учитывались все изменения:
         fs.readdir('./04-copy-directory/files', {withFileTypes: true}, (err, files) => {
           if(err) {
             throw err
